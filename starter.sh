4d061570bf2b1ebabd55c031f9d9eae9d9c528d9
Zach A. Thomas <zach@me.com>
2017-03-03 20:48:23 -0500

    Update starter.sh
    use desktop.yml and remove purecloud.yml

--- starter.sh
+++ starter.sh
@@ -5,4 +5,4 @@ sudo pip install ansible
 mkdir ~/dev
 git clone https://github.com/dysmento/starter.git ~/dev/starter
 cd ~/dev/starter
-ansible-galaxy install -r requirements.yml && ansible-playbook --ask-vault-pass --ask-become-pass purecloud.yml
+ansible-galaxy install -r requirements.yml && ansible-playbook --ask-vault-pass --ask-become-pass desktop.yml
