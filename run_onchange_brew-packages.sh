db4dcd56ee35c938f77faeb8eaf7d3a8d368f9d9
Zach A. Thomas <zach.thomas@dividendfinance.com>
2025-02-08 11:44:01 -0500

    maybe script work now

--- run_onchange_brew-packages.sh
+++ run_onchange_brew-packages.sh
@@ -1,19 +1,2 @@
-#!/bin/sh
-brew install   \ 
-bat            \
-cloc           \
-direnv         \
-docker         \
-docker-compose \
-fish           \
-fzf            \
-glow           \
-gum            \
-httpie         \
-jq             \
-lsd            \
-shellcheck     \
-sqlite         \
-starship       \
-uv             \
-wget
+#!/bin/bash
+brew install bat cloc direnv docker docker-compose fish fzf glow gum httpie jq lsd shellcheck sqlite starship uv wget
